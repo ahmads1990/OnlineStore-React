8d3d010f101c4da76be20612a60ac46f6e404735
Ahmad <ahmads20002000@gmail.com>
2024-07-01 19:13:28 +0300

    Update productList layout

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,11 +3,22 @@ import productData from "../productData.js";
 
 const ProductList = () => {
     return (
-        <div className="product-list container">
+        <div className="container">
             <div className="row">
-                {productData.map((product) => (
-                    <ProductItem key={product.Id} productData={product} />
-                ))}
+                <div className="col-3">
+                    <div>Filter by price</div>
+                    <div>Filter by price</div>
+                    <div>Filter by price</div>
+                    <div>Filter by price</div>
+                </div>
+                <div className="col">
+                    <div className="row px-3 mb-3">{productData.length} Products</div>
+                    <div className="row">
+                        {productData.map((product) => (
+                            <ProductItem key={product.Id} productData={product} />
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
