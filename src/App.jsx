ad72bf691eb0ad1c8d5f6dbe2f10457117cd5037
Ahmad <ahmads20002000@gmail.com>
2024-07-01 18:05:04 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+    return (
+        <>
+            <h1 className="text-bg-danger">Hello world!</h1>
+            <div>First version</div>
+        </>
+    );
+};
+
+export default App;
