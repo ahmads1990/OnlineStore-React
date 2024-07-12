e0e5dfd6088d2a7803be6e2fc67f504a83ec0de7
Ahmad <ahmads20002000@gmail.com>
2024-07-13 00:53:04 +0300

    Add link to productDetails in Item comp

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "./ProductItem.css";
 
 const ProductItem = ({ productData }) => {
     return (
         <div className="product col-3 mb-4">
             <div className="card">
-                <img className="card-img-top" src={`./${productData.MainImage}`} alt="" />
+                <Link to={`/product/${productData.Id}`}>
+                    <img className="card-img-top" src={`./${productData.MainImage}`} alt="" />
+                </Link>
 
                 <div className="card-body">
                     <h6 className="card-title">{productData.Title}</h6>
