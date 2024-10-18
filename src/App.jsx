e024bd48fee0f226092f46921eacc71b00031341
ahmads1990 <ahmads20002000@gmail.com>
2024-10-18 19:22:56 +0300

    Add layout comp and move navbar to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import ProductList from "./components/ProductList/ProductList";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
-import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AddProductForm from "./components/AddProductForm/AddProductForm";
+import Layout from "./components/Layout/Layout";
 
 const App = () => {
     return (
         <>
-            <Navbar />
             <Routes>
-                <Route exact path="/" element={<ProductList />} />
-                <Route path="/product/:id" element={<ProductDetails />} />
-                <Route path="/add" element={<AddProductForm />} />
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<ProductList />} />
+                    <Route path="product/:id" element={<ProductDetails />} />
+                    <Route path="add" element={<AddProductForm />} />
+                </Route>
             </Routes>
         </>
     );
