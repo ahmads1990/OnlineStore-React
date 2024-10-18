7f653f186c3c04ec50dc9ab16454c9bac0acfc26
ahmads1990 <ahmads20002000@gmail.com>
2024-10-18 20:15:31 +0300

    Update Navbar UI and use router links

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,10 +1,10 @@
+import { Cart, Person, Search, ShopWindow } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-secondary mb-4">
-            <div className="container-fluid">
-                <a href="" className="navbar-brand">
-                    Online Store
-                </a>
+            <div className="container-fluid d-flex">
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -17,45 +17,49 @@ const Navbar = () => {
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
+                <Link to="/" className="navbar-brand d-flex align-items-center px-6 mx-auto">
+                    <ShopWindow className="me-1" />
+                    Online Store
+                </Link>
+
+                <div className="d-flex align-items-center order-lg-1 ml-auto px-2 pr-2">
+                    <Link className="nav-link text-capitalize me-2" to="/">
+                        <Search className="fs-4 text-primary" />
+                    </Link>
+                    <Link className="nav-link text-capitalize me-2" to="/">
+                        <Person className="fs-2 text-primary" />
+                    </Link>
+                    <Link className="nav-link text-capitalize me-2" to="/">
+                        <Cart className="fs-4 text-primary" />
+                    </Link>
+                </div>
+
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto ms-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">
+                            <Link className="nav-link active text-capitalize" aria-current="page" to="/">
                                 Home
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="add">
-                                Link
-                            </a>
+                            <Link className="nav-link active text-capitalize" to="/">
+                                Best Collection
+                            </Link>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
+                        <li className="nav-item">
+                            <Link className="nav-link active text-capitalize" to="/">
+                                Shop All Snacks
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active text-capitalize" to="/">
+                                Sale!
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active text-capitalize" to="add">
+                                Add Snack
+                            </Link>
                         </li>
                     </ul>
                 </div>
