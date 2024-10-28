2900cbe3b0d50ab3287a5d74b5599a5f0a946660
ahmads1990 <ahmads20002000@gmail.com>
2024-10-28 05:34:31 +0300

    Update productList page

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,5 +1,6 @@
 import ProductItem from "../ProductItem/ProductItem.jsx";
 import useFetchProducts from "../../hooks/useFetchProducts.js";
+import { SquareFill } from "react-bootstrap-icons";
 
 const ProductList = () => {
     const { data: productData, isLoading, error } = useFetchProducts();
@@ -7,14 +8,38 @@ const ProductList = () => {
     return (
         <div className="container-fluid px-5">
             <div className="row">
+                <hr />
                 <div className="col-2">
-                    <div>Filter by price</div>
-                    <div>Filter by price</div>
-                    <div>Filter by price</div>
-                    <div>Filter by price</div>
+                    <div>By Price</div>
+                    <div>By Giftee</div>
+                    <div>By Occasion</div>
+                    <div>By Flavor</div>
+                    <div>By Diet</div>
                 </div>
                 <div className="col">
-                    <div className="row px-3 mb-3">{productData.length} Products</div>
+                    <div className="row px-3 mb-3">
+                        <div className="d-flex justify-content-between align-content-center">
+                            <div>{productData.length} Products</div>
+
+                            <div className="d-flex align-content-center">
+                                <select
+                                    className="form-select form-select-sm py-0"
+                                    aria-label="Small select example"
+                                    style={{ height: "2rem" }}
+                                >
+                                    <option selected>Featured</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                                <div className="d-flex">
+                                    <SquareFill />
+                                    <SquareFill />
+                                    <SquareFill />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="row g-3">
                         {isLoading && (
                             // Loading spinner
